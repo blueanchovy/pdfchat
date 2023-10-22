ece9e17761a73042e697a81bd3fce34d1bc2b224
Manish Jha <manishjhazyx@gmail.com>
2023-10-22 15:59:52 +0530

    feat: homepage - hero section, steps section

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <section
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default MaxWidthWrapper;
